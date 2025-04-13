ef69692f18c3ad98c692df344578aac20b68be31
claudiabraida <braidacreaciones@gamail.com>
2025-04-12 23:04:10 -0300

    add: header, hero, components filters

--- 3main.js
+++ 3main.js
@@ -1,5 +1,28 @@
-import {} from "./2features.js"
+import {
+    
+} from "./2features.js"
 
+/* ----------📌📌📌📌📌 GENERAL SELECTORS 📌📌📌📌📌---------- */
+const $ = element => document.querySelector(element);
+const $$ = element => document.querySelectorAll(element);
+
+const hideElement = (selectors) => {
+  for (const selector of selectors) {
+    selector.classList.add("hidden");
+  }
+};
+
+const showElement = (selectors) => {
+  for (const selector of selectors) {
+    selector.classList.remove("hidden");
+  }
+};
+
+const showAndHideElement = (selectors) => {
+  for (const selector of selectors) {
+    selector.classList.toggle("hidden");
+  }
+};
 
 
 window.onload = async () => {
